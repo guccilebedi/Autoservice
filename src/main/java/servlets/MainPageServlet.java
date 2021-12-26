e498f549941f6ce024cf43552a8c756439effc45
Danila Lebedev <daniilvlebedev@gmail.com>
2021-12-26 18:50:08 +0300

    United MasterMainPageServlet and ManagerMainPageServlet into MainPageServlet

--- src/main/java/servlets/MainPageServlet.java
+++ src/main/java/servlets/MainPageServlet.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 
 import static java.util.Objects.nonNull;
 
-public class ManagerMainPageServlet extends HttpServlet {
+public class MainPageServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException { // метод get
         final HttpSession session = request.getSession();
@@ -29,7 +29,7 @@ public class ManagerMainPageServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException { // метод post
         final HttpSession session = request.getSession();
         if (nonNull(session)) { // проверка того, что пользователь авторизован
-            if (session.getAttribute("role") == Role.MANAGER) { // если менеджер - перенаправить на страницу регистрации
+            if (session.getAttribute("role") == Role.MANAGER) {
                 if (request.getParameter("Register") != null) {
                     request.getRequestDispatcher("/WEB-INF/view/register.jsp").forward(request, response);
                 } else if (request.getParameter("Delete") != null) {
@@ -37,8 +37,8 @@ public class ManagerMainPageServlet extends HttpServlet {
                 } else if (request.getParameter("AddCustomer") != null) {
                     request.getRequestDispatcher("/WEB-INF/view/manager_add_customers_visit.jsp").forward(request, response);
                 }
-            } else if (session.getAttribute("role") == Role.MASTER) { // если мастер - перенаправить на его главную страницу
-                request.getRequestDispatcher("/WEB-INF/view/master_main_page.jsp").forward(request, response);
+            } else if (session.getAttribute("role") == Role.MASTER) {
+                request.getRequestDispatcher("/WEB-INF/view/master_add_customers_visit.jsp").forward(request, response);
             }
         } else { // иначе перенаправить на страницу входа
             request.getRequestDispatcher("/WEB-INF/view/login.jsp").forward(request, response);
