a2d70001c2714cb7a26035a8e074fa7788c22697
Danila Lebedev <daniilvlebedev@gmail.com>
2021-12-26 15:36:57 +0300

    Added comments, back button in registration, some fixes

--- src/main/java/servlets/ManagerMainPageServlet.java
+++ src/main/java/servlets/ManagerMainPageServlet.java
@@ -15,26 +15,26 @@ public class ManagerMainPageServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException { // метод get
         final HttpSession session = request.getSession();
-        if (nonNull(session)) {
-            if (session.getAttribute("role") == Role.MANAGER) {
+        if (nonNull(session)) { // проверка того, что пользователь авторизован
+            if (session.getAttribute("role") == Role.MANAGER) { // если менеджер - перенаправить на его главную страницу
                 request.getRequestDispatcher("/WEB-INF/view/manager_main_page.jsp").forward(request, response);
-            } else if (session.getAttribute("role") == Role.MASTER) {
+            } else if (session.getAttribute("role") == Role.MASTER) { // если мастер - перенаправить на его главную страницу
                 request.getRequestDispatcher("/WEB-INF/view/master_main_page.jsp").forward(request, response);
             }
-        } else {
+        } else { // иначе перенаправить на страницу входа
             request.getRequestDispatcher("/WEB-INF/view/login.jsp").forward(request, response);
         }
     }
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException { // метод post
         final HttpSession session = request.getSession();
-        if (nonNull(session)) {
-            if (session.getAttribute("role") == Role.MANAGER) {
-                request.getRequestDispatcher("/WEB-INF/view/manager_main_page.jsp").forward(request, response);
-            } else if (session.getAttribute("role") == Role.MASTER) {
+        if (nonNull(session)) { // проверка того, что пользователь авторизован
+            if (session.getAttribute("role") == Role.MANAGER) { // если менеджер - перенаправить на страницу регистрации
+                request.getRequestDispatcher("/WEB-INF/view/register.jsp").forward(request, response);
+            } else if (session.getAttribute("role") == Role.MASTER) { // если мастер - перенаправить на его главную страницу
                 request.getRequestDispatcher("/WEB-INF/view/master_main_page.jsp").forward(request, response);
             }
-        } else {
+        } else { // иначе перенаправить на страницу входа
             request.getRequestDispatcher("/WEB-INF/view/login.jsp").forward(request, response);
         }
     }
--- src/main/java/servlets/MasterMainPageServlet.java
+++ src/main/java/servlets/MasterMainPageServlet.java
@@ -15,13 +15,13 @@ public class MasterMainPageServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException { // метод get
         final HttpSession session = request.getSession();
-        if (nonNull(session)) {
-            if (session.getAttribute("role") == Role.MANAGER) {
+        if (nonNull(session)) { // проверка того, что пользователь авторизован
+            if (session.getAttribute("role") == Role.MANAGER) { // если менеджер - перенаправить на его главную страницу
                 request.getRequestDispatcher("/WEB-INF/view/manager_main_page.jsp").forward(request, response);
-            } else if (session.getAttribute("role") == Role.MASTER) {
+            } else if (session.getAttribute("role") == Role.MASTER) { // если мастер - перенаправить на его главную страницу
                 request.getRequestDispatcher("/WEB-INF/view/master_main_page.jsp").forward(request, response);
             }
-        } else {
+        } else { // иначе перенаправить на страницу входа
             request.getRequestDispatcher("/WEB-INF/view/login.jsp").forward(request, response);
         }
     }
